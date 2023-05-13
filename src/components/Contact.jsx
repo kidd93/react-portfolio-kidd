ff96ed46817b8b5323c2331cf0d2e63abd2ee29a
kelsie kidd <kelsiekidd93@gmail.com>
2023-05-13 02:47:59 -0500

    CSS add ons

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,10 +10,10 @@ const Contact = () => {
           <p className='text-3xl flex justify-center items-center px-5 py-5 pb-4 text-cyan-500 font-bold'>Contact</p>
           <p className='px-5 pb-5 text-gray-100'>I hope you enjoyed my portfolio. If you have any comments or questions please fill out this contact form. Thank you for your time!</p>
         </div>
-        <input className='p-3 rounded-lg text-black text-lg shadow-lg shadow-black' type='text' placeholder='Name' name='name'></input>
-        <input className='my-2 p-3 rounded-lg text-black text-lg shadow-lg shadow-black' type='text' placeholder='Email' name='email'></input>
-        <textarea className='p-3 rounded-lg text-black text-lg shadow-lg shadow-black pb-5' rows='10' placeholder='Message' name='message'></textarea>
-        <button className='my-2 mx-auto flex items-center shadow-lg border-2 border-zinc-400 shadow-black px-8 py-3 hover:bg-sky-900 hover:border-sky-900 font-semibold'>Send</button>
+        <input className='p-2 m-5 rounded-lg text-black text-lg shadow-lg shadow-black' type='text' placeholder='Name' name='name'></input>
+        <input className='my-1 p-2 m-5 rounded-lg text-black text-lg shadow-lg shadow-black' type='text' placeholder='Email' name='email'></input>
+        <textarea className='p-2 m-5 rounded-lg text-black text-lg shadow-lg shadow-black pb-5' rows='10' placeholder='Message' name='message'></textarea>
+      <button className='my-1 mx-auto flex items-center border-2 border-gray-100 shadow-lg shadow-black px-5 py-3 hover:bg-cyan-700 hover:border-cyan-800 font-semibold'>Send</button>
       </form>
     </div>
   )
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
+import bg from '../assets/background.png'
 import soon from '../assets/comingsoon.jpg'
 import styleBuddy from '../assets/styleBuddy2.png'
 // import svelte from '../assets/SvelteCreationForm.png'
 import booksearch from '../assets/booksearch.png'
 import Doggypedia from '../assets/Doggypedia.png'
-import Jate from '../assets/Jate.png'
 
 const Projects = () => {
   return (
-    <div className='w-full md:h-screen bg-slate-500 text-zinc-300' name='projects'>
+    <div className='w-full md:h-screen bg-slate-500 text-zinc-300' style={{ backgroundImage: `url(${bg})`, backgroundRepeat: `no-repeat`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundAttachment: 'fixed' }} name='projects'>
       <div className='flex flex-col justify-center place-items-center w-full h-full'>
-        <div className='flex flex-col justify-center place-items-center max-w-[800px] bg-slate-700 rounded-lg'>
-          <p className='text-3xl py-8'>Projects</p>
-          <h1 className=' md:text-lg pb-5 px-5'>Please click below to view/demo my bootcamp projects. All applications are deployed through Heroku. A few are collaborations that I helped contribute towards and enjoyed working on. Thank you!</h1>
+        <div className='flex flex-col justify-center place-items-center max-w-[800px] lg:rounded-xl shadow-black shadow-xl bg-gradient-to-tr from-[#181818] to-[#707070]'>
+          <p className='text-3xl text-cyan-500 font-bold px-5 py-5 pb-4'>Projects</p>
+          <h1 className=' md:text-lg pb-5 px-5'>Please click below to view/demo my bootcamp projects. All applications are deployed through Heroku. A few are collaborations that I helped contribute towards and enjoyed working on. To see the code for my projects please visit my GitHub. The link is located at the top. Thank you!</h1>
 
           <div className='flex flex-col justify-center mt-10'>
             <div className='grid grid-cols-1 lg:grid-cols-2 gap-4'>
@@ -36,7 +36,7 @@ const Projects = () => {
             <div className='grid grid-cols-1 lg:grid-cols-2 gap-4'>
               <div>
                 
-                  <img src={soon} alt='' style={{ width: '350px', height: '250px'}} className='shadow-xl shadow-black hover:shadow-zinc-400'>
+                  <img src={soon} alt='' style={{ width: '300px', height: '200px'}} className='shadow-xl shadow-black hover:shadow-zinc-400'>
                   </img>
                
               </div>
